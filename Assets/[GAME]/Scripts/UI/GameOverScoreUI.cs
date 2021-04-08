8b3178cccf463f95343ea257aa32c6578f3647e1
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2021-04-09 01:01:33 +0300

    +add ScoreTextUI, GameOverScoreUI, RestartButton

---
+++ Assets/[GAME]/Scripts/UI/GameOverScoreUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScoreUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text _scoreText;
+    [SerializeField]
+    private Text _highScoreText;
+
+    private void OnEnable()
+    {
+        if (Managers.Instance == null)
+            return;
+        EventManager.OnEndScoreUpdate.AddListener(EndScoreTextUpdate);
+    }
+
+    private void OnDisable()
+    {
+        if (Managers.Instance == null)
+            return;
+        EventManager.OnEndScoreUpdate.RemoveListener(EndScoreTextUpdate);
+    }
+
+    private void EndScoreTextUpdate()
+    {
+        _scoreText.text = "Your Score: " + ScoreManager.Instance.score.ToString("0.00");
+        _highScoreText.text = "Highest Score: " + PlayerPrefs.GetFloat("HighScore").ToString("0.00");
+    }
+}
---
+++ Assets/[GAME]/Scripts/UI/ScoreTextUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTextUI : MonoBehaviour
+{
+    private Text _scoreText;
+    private void OnEnable()
+    {
+        if (Managers.Instance == null)
+            return;
+        _scoreText = GetComponent<Text>(); 
+        EventManager.OnScoreUIUpdate.AddListener(ScoreTextUpdate);
+    }
+
+    private void OnDisable()
+    {
+        if (Managers.Instance == null)
+            return; 
+        EventManager.OnScoreUIUpdate.RemoveListener(ScoreTextUpdate);
+    }
+
+    private void ScoreTextUpdate()
+    {
+        _scoreText.text = ScoreManager.Instance.score.ToString("0.00");
+    }
+
+}
