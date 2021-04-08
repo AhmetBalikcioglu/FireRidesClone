a0f4caa4215938f1d1c98dcca874acccb48b9a9e
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2021-04-09 01:00:56 +0300

    +add Panel, GameOverPanel, InGamePanel, StartGamePanel

---
+++ Assets/[GAME]/Scripts/UI/Panel/GameOverPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverPanel : Panel
+{
+    private void OnEnable()
+    {
+        if (Managers.Instance == null)
+            return;
+        EventManager.OnGameStart.AddListener(HidePanel);
+        EventManager.OnLevelFinish.AddListener(ShowPanel);
+    }
+
+    private void OnDisable()
+    {
+        if (Managers.Instance == null)
+            return;
+        EventManager.OnGameStart.RemoveListener(HidePanel);
+        EventManager.OnLevelFinish.RemoveListener(ShowPanel);
+    }
+}
