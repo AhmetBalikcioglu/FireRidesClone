2b911626d3943628def085585079d3da0e958d2d
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2021-04-09 00:58:04 +0300

    +add Character

---
+++ Assets/[GAME]/Scripts/Character/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum CharacterControllerType { Player, AI }
+
+public class Character : MonoBehaviour
+{
+    public CharacterControllerType CharacterControllerType = CharacterControllerType.Player;
+
+    private Collider collider;
+    public Collider Collider { get { return (collider == null) ? collider = GetComponent<Collider>() : collider; } }
+
+    private Rigidbody rigidbody;
+    public Rigidbody Rigidbody { get { return (rigidbody == null) ? rigidbody = GetComponent<Rigidbody>() : rigidbody; } }
+
+    private bool isControlable;
+    public bool IsControlable { get { return isControlable; } set { isControlable = value; } }
+
+    private void OnEnable()
+    {
+        if (Managers.Instance == null)
+            return;
+
+        CharacterManager.Instance.AddCharacter(this);
+    }
+
+    private void OnDisable()
+    {
+        if (Managers.Instance == null)
+            return;
+
+        CharacterManager.Instance.RemoveCharacter(this);
+    }
+}
