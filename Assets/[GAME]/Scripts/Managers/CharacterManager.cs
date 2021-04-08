d9c5fd353e8aec07f6f5c98b0e6e652c9536f67d
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2021-04-09 01:00:06 +0300

    +add CharacterManager, EventManager, GameManager, InputManager, Managers, ScoreManager

---
+++ Assets/[GAME]/Scripts/Managers/CharacterManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : Singleton<CharacterManager>
+{
+    private List<Character> characters;
+    public List<Character> Characters { get { return (characters == null) ? characters = new List<Character>() : characters; } set { characters = value; } }
+
+    private Character player;
+    public Character Player
+    {
+        get
+        {
+            if (player == null)
+            {
+                foreach (var character in Characters)
+                {
+                    if (character.CharacterControllerType == CharacterControllerType.Player)
+                        player = character;
+                }
+            }
+
+            return player;
+        }
+
+        set
+        {
+            player = value;
+        }
+    }
+
+    private List<Character> aiCharacters;
+    public List<Character> AICharacters
+    {
+        get
+        {
+            if (aiCharacters == null || aiCharacters.Count == 0)
+            {
+                foreach (var character in Characters)
+                {
+                    if (character.CharacterControllerType == CharacterControllerType.AI)
+                    {
+                        if (!aiCharacters.Contains(character))
+                            aiCharacters.Add(character);
+                    }
+                }
+            }
+
+            return aiCharacters;
+        }
+
+        set
+        {
+            aiCharacters = value;
+        }
+    }
+    public void AddCharacter(Character character)
+    {
+        if (!Characters.Contains(character))
+            Characters.Add(character);
+    }
+
+    public void RemoveCharacter(Character character)
+    {
+        if (Characters.Contains(character))
+            Characters.Remove(character);
+    }
+}
---
+++ Assets/[GAME]/Scripts/Managers/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class EventManager
+{
+    public static UnityEvent OnGameStart = new UnityEvent();
+    public static UnityEvent OnGameEnd = new UnityEvent();
+    public static UnityEvent OnGameRestart = new UnityEvent();
+    public static UnityEvent OnSceneLoad = new UnityEvent();
+
+    public static UnityEvent OnLevelStart = new UnityEvent();
+    public static UnityEvent OnLevelFinish = new UnityEvent();
+
+    public static UnityEvent OnLevelSuccess = new UnityEvent();
+    public static UnityEvent OnLevelFail = new UnityEvent();
+
+    public static UnityEvent OnScoreUIUpdate = new UnityEvent();
+    public static UnityEvent OnEndScoreUpdate = new UnityEvent();
+
+    public static UnityEvent OnMouseDown = new UnityEvent();
+    public static UnityEvent OnMouseUp = new UnityEvent();
+
+
+}
+public class InputEvent : UnityEvent<float> { }
