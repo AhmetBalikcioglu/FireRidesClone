ac3ecf2c8c32a38b64790f50fb4a45fda1c9df36
AhmetBalikcioglu <69127340+AhmetBalikcioglu@users.noreply.github.com>
2021-04-11 20:11:32 +0300

    +add ICollectable, Ring

---
+++ Assets/[GAME]/Scripts/Ring.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour, ICollectable
+{
+    public void Collect()
+    {
+        if (Vector3.Distance(CharacterManager.Instance.transform.position, transform.position) < .5f)
+        {
+            ScoreManager.Instance.score += 10f;
+        }
+        else
+        {
+            ScoreManager.Instance.score += 5f;
+        }
+        Destroy(transform.parent.gameObject);
+    }
+}
